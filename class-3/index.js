fca75c226ec7d7fe20a4baf3cf15da30902bb7e7
Sufyan-Naveed <sufyansnaved123@gmail.com>
2024-08-28 08:41:18 -0700

    Class 3

---
+++ class-3/index.js
@@ -0,0 +1,54 @@
+// var number = +prompt("Enter number here: ")
+
+// alert(number % 2)
+
+// var hassanSufyan
+// var hassansufyan
+// var hassan_sufyan
+// // var class
+// // var function
+
+
+// var classFuntion
+// var class_fu
+// // var hassan sufyan // wrong
+
+
+// var NUMBER = 10
+
+// alert(10)
+// alert(NUMBER + 10)
+
+// var userInputNumber;
+// var name = "Saad"
+
+
+// var num = 10;
+// var popularNumber = num + 200;
+
+// alert(popularNumber)
+
+
+// var a = 10
+// var b = 20
+// var c = a + b // 30
+
+// var d = ((a * b) * c) + b // 6020
+
+// alert(d)
+
+
+var a = 10
+
+
+alert(a) // 10
+// alert(++a + a) // 10
+// alert(a) // 11
+
+
+// alert(a++ + a++) // 21
+alert(++a) // 13
+
+
+alert(a--) // 13
+alert(a) // 12
